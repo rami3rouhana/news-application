dfc9a4db707aa476941088958689e6d86f9fcc42
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-05 16:02:06 +0300

    Add alan logo

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from "react";
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from './styles'
+import alanLogo from './images/alanLogo.png'
 
 const App = () => {
   const [newsArticales, setNewsArticales] = useState('');
+  const classes = useStyles();
+
   useEffect(() => {
     alanBtn({
       key: '1a2935cb42fe364b1dcfe5cd4c05318f2e956eca572e1d8b807a3e2338fdd0dc/stage',
@@ -18,7 +22,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>Alan AI News Application</h1>
+      <div className={classes.logoContainer}>
+        <img src={alanLogo} className={classes.alanLogo} alt="alan logo"/>
+      </div>
       <NewsCards articles={newsArticales} />
     </div>
   );
